575155f90ab35ad6b03cd6fcf10a41f121613dd1
kavya-gongati <36591559+kavya-gongati@users.noreply.github.com>
2018-03-04 12:21:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ var element=document.getElementById('main'
 );
 element.innerHTMl='new';
 var img = document.getElementById('madi');
+
+
+var marginleft =0;
+function movieRight () {
+ marginleft = marginleft + 10;
+ img.style.marginleft = marginleft + 'px';
+}
 img.onClick = function () {
-    img.style.marginleft = '100px';
+    var interval = setInterval(moveRight, 100);
 };
