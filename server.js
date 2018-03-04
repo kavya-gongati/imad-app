b1605f5a1cc1b9d59eff8af644b594928a2eb83f
kavya-gongati <36591559+kavya-gongati@users.noreply.github.com>
2018-03-04 11:30:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,50 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone={
+    heading:"article one",
+    title:"article-one",
+    date:"sept 5",
+    content:`<p>A paragraph is a group of words put together to form a group that is usually longer than a sentence. Paragraphs are often made up of many sentences. They are usually between four to eight sentences. Paragraphs can begin with an indentation (about five spaces), or by missing a line out, and then starting again; this makes 
+            </p>
+            <p>A paragraph is a group of words put together to form a group that is usually longer than a sentence. Paragraphs are often made up of many sentences. They are usually between four to eight sentences. Paragraphs can begin with an indentation (about five spaces), or by missing a line out, and then starting again; this makes </p>
+            <p>A paragraph is a group of words put together to form a group that is usually longer than a sentence. Paragraphs are often made up of many sentences. They are usually between four to eight sentences. Paragraphs can begin with an indentation (about five spaces), or by missing a line out, and then starting again; this makes </p>
+            
+    `
+};
+function createtemplate(data){
+  var title=data.title;
+  var heading=data.heading;
+  var date=data.date;
+  var content=data.content;
 
+        var htmltemplate=`<html>
+            <head>
+                <title>${title}</title>
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body><div class="container">
+                <div>
+                    <a href='/'>home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>${date}</div>
+                <div>
+                    ${content}
+                </div>
+                </div>
+            </body>
+        </html>
+        
+        `;
+        return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(articleone));
 });
 app.get('/article-two',function(req, res){
      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
