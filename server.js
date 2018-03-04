94f5f29342bcd7406aa3ddb57b7abcb416ec6531
kavya-gongati <36591559+kavya-gongati@users.noreply.github.com>
2018-03-04 11:42:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articlename',function(req, res){
-    var articlename=req.parans.articlename;
+    var articlename=req.params.articlename;
     res.send(createtemplate(articles[articlename]));
 });
 
