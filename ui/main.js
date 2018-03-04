eadaae7aa3d12e42b86c390a87862f2fd7658ece
kavya-gongati <36591559+kavya-gongati@users.noreply.github.com>
2018-03-04 12:13:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main'
 );
 element.innerHTMl='new';
+var img = document.getElemrntById('madi');
+img.onClick = function () {
+    img.style.marginleft = '100px';
+};
